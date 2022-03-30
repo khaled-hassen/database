8cdf74fef4fdf42ac71ba3821225a15b49c96d11
khaled <khaledhassen.2015@gmail.com>
2022-03-31 00:58:00 +0200

    setup and configure clion to use wxWidgets

--- main.cpp
+++ main.cpp
@@ -1,45 +1,83 @@
-#include <iostream>
-#include "src/CLI/CLI.h"
-#include "src/Utils/Pointer/Pointer.h"
-#include "src/Database/Database.h"
+#include <wx/wxprec.h>
 
-int main()
+// For compilers that support precompilation, includes "wx/wx.h".
+#ifndef WX_PRECOMP
+
+#include <wx/wx.h>
+
+#endif
+
+class MyApp : public wxApp
 {
-    // CLI cli;
-    Pointer<Database> db(new Database());
-    // while (true)
-    // {
-    try
-    {
-        // db->CreateDb("users");
-        db->OpenDb("users");
-        // db->DropTable("table2");
-        // db->CreateTable("table2");
-        // db->GetTable()->Save();
-        // db->CloseTable();
-        // db->OpenTable("table1");
-        db->OpenTable("table2");
-        // db->GetTable()->SearchRecord();
-        db->GetTable()->UpdateRecord(5);
-        db->GetTable()->Save();
-        db->CloseTable();
-        // db->GetTable()->ShowRecords();
-        // db->GetTable()->InsertRecord();
-        // db->GetTable()->DeleteRecord(2);
-        // db->OpenTable("table2");
-        // db->GetTable()->ShowRecords();
-        // db->ShowTables();
-        // db->DropDb();
-        // db->DropDb("users");
-        db->CloseDb();
-        // cli.listen();
-        // cli.execute();
-    }
-    catch (const std::exception& error)
-    { std::cout << error.what() << std::endl; }
+public:
+    bool OnInit() override;
+};
 
-    // if (cli.isClosed()) break;
-    // }
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
 
-    return 0;
+private:
+    void OnHello(wxCommandEvent& event);
+
+    void OnExit(wxCommandEvent& event);
+
+    void OnAbout(wxCommandEvent& event);
+};
+
+enum
+{
+    ID_Hello = 1
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame()
+        : wxFrame(NULL, wxID_ANY, "Hello World")
+{
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+
+    wxMenu* menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+
+    SetMenuBar(menuBar);
+
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets Hello World example",
+                 "About Hello World", wxOK | wxICON_INFORMATION);
+}
+
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
 }
